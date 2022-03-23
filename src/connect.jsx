a983e8daf03da11f8d2895975060f16908b11279
José Gregorio Andradez <herrkin@Joses-MacBook-Pro-2.local>
2022-03-23 11:46:12 -0300

    Updates

--- src/connect.jsx
+++
@@ -1,7 +0,0 @@
-
-// import { ConnectSample } from 'components/ConnectSample'
-// import react, { useEffect } from 'react'
-
-// export const Connect = () => {
-//     return   <ConnectSample />
-// }
